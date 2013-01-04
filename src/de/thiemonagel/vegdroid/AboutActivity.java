b0748e881071d601bfe1ec29840793ddfcec89f1
Thiemo Nagel <thiemo.nagel@gmail.com>
2013-01-04 18:37:36 +0100

    Display Google Maps licensing in AboutActivity
    Convert text_about resource string to proper HTML on that occasion.

--- src/de/thiemonagel/vegdroid/AboutActivity.java
+++ src/de/thiemonagel/vegdroid/AboutActivity.java
@@ -1,15 +1,36 @@
 package de.thiemonagel.vegdroid;
 
-import android.os.Bundle;
-import android.view.View;
 import android.app.Activity;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 public class AboutActivity extends Activity {
+
+    private static final String                      LOG_TAG       = "VegDroid";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        // display Google Maps API licensing info, as required in
+        // https://developers.google.com/maps/documentation/android/intro
+        {
+            TextView tv = (TextView) findViewById(R.id.aboutText);
+            String s = getString(R.string.text_about);
+            Log.d( LOG_TAG, "About string: " + s );
+            tv.setText( Html.fromHtml(getString(R.string.text_about)) );
+            tv.setMovementMethod( LinkMovementMethod.getInstance() );
+        }{
+            TextView tv = (TextView) findViewById(R.id.GMapLegalese);
+            tv.setText( GooglePlayServicesUtil.getOpenSourceSoftwareLicenseInfo( this ) );
+        }
     }
 
     /*
