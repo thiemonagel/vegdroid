9a9508320cb7f6d613374ff01f804131c16fb44f
Thiemo Nagel <thiemo.nagel@googlemail.com>
2013-01-16 18:28:25 +0100

    Minimal refactoring

--- src/de/thiemonagel/vegdroid/MapActivity.java
+++ src/de/thiemonagel/vegdroid/MapActivity.java
@@ -221,76 +221,77 @@ public class MapActivity extends android.support.v4.app.FragmentActivity {
 
 
     private void setupMap() {
-        if ( fMap == null ) {
-            fMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
-            fMap.setMyLocationEnabled(true);
-            UiSettings ui = fMap.getUiSettings();
-            ui.setCompassEnabled(true);
-            ui.setMyLocationButtonEnabled(true);
-            ui.setRotateGesturesEnabled(true);
-            ui.setScrollGesturesEnabled(true);
-            ui.setTiltGesturesEnabled(true);
-            ui.setZoomControlsEnabled(true);
-            ui.setZoomGesturesEnabled(true);
+        if ( fMap != null )
+            return;
 
-            if ( MyData.getInstance().UpdateLocation() )
-                fMap.moveCamera( CameraUpdateFactory.newLatLngZoom( MyData.getInstance().getLocation(), 12.f) );
+        fMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        fMap.setMyLocationEnabled(true);
+        UiSettings ui = fMap.getUiSettings();
+        ui.setCompassEnabled(true);
+        ui.setMyLocationButtonEnabled(true);
+        ui.setRotateGesturesEnabled(true);
+        ui.setScrollGesturesEnabled(true);
+        ui.setTiltGesturesEnabled(true);
+        ui.setZoomControlsEnabled(true);
+        ui.setZoomGesturesEnabled(true);
 
-            /*
-            Date gstart = new Date();
-            Geocoder gc = new Geocoder(this);
-            int N = 1;
-            for ( int i=1; i<=N; i++ )
-                try {
-                    String loc = "Leopoldstr. " + i + ", München";
+        if ( MyData.getInstance().UpdateLocation() )
+            fMap.moveCamera( CameraUpdateFactory.newLatLngZoom( MyData.getInstance().getLocation(), 12.f) );
 
-                    Date start = new Date();
-                    List<Address> la = gc.getFromLocationName(loc, 1);
-                    Date end = new Date();
-                    float ms = (end.getTime()-start.getTime());
-                    Log.d( LOG_TAG, "geocode " + i + ": " + ms + " ms" );
+        /*
+        Date gstart = new Date();
+        Geocoder gc = new Geocoder(this);
+        int N = 1;
+        for ( int i=1; i<=N; i++ )
+            try {
+                String loc = "Leopoldstr. " + i + ", München";
 
-                    if ( la.size() > 0 ) {
-                        Address a = la.get(0);
-                        fMap.addMarker( new MarkerOptions()
-                                .position( new LatLng(a.getLatitude(), a.getLongitude()) )
-                                .title("Title")
-                                .snippet(loc)
-                        );
-                    }
-                } catch (IOException e) {};
-
+                Date start = new Date();
+                List<Address> la = gc.getFromLocationName(loc, 1);
                 Date end = new Date();
-                float ms = (end.getTime()-gstart.getTime());
-                Log.d( LOG_TAG, "avg time per geocode: " + ms/N + " ms" );
-            */
+                float ms = (end.getTime()-start.getTime());
+                Log.d( LOG_TAG, "geocode " + i + ": " + ms + " ms" );
 
-
-
-            /*
-            ArrayList<Float> lat = new ArrayList<Float>();
-            ArrayList<Float> lon = new ArrayList<Float>();
-            int N=1000;
-            for ( int i=0; i<N; i++ ) {
-                float la = 48.139126f;
-                float lo = 11.580186f;
-                for ( int j=0; j<20; j++ ) {
-                    la += (Math.random()-0.5)/30;
-                    lo += (Math.random()-0.5)/30;
+                if ( la.size() > 0 ) {
+                    Address a = la.get(0);
+                    fMap.addMarker( new MarkerOptions()
+                            .position( new LatLng(a.getLatitude(), a.getLongitude()) )
+                            .title("Title")
+                            .snippet(loc)
+                    );
                 }
-                lat.add(la);
-                lon.add(lo);
-            }
+            } catch (IOException e) {};
 
-            for ( int i=0; i<N; i++ ) {
-                fMap.addMarker( new MarkerOptions()
-                        .position( new LatLng(lat.get(i), lon.get(i)) )
-                        .title("Title "+i)
-                        .snippet("Text "+i)
-                        );
-            }
-            */
+            Date end = new Date();
+            float ms = (end.getTime()-gstart.getTime());
+            Log.d( LOG_TAG, "avg time per geocode: " + ms/N + " ms" );
+        */
 
+
+
+        /*
+        ArrayList<Float> lat = new ArrayList<Float>();
+        ArrayList<Float> lon = new ArrayList<Float>();
+        int N=1000;
+        for ( int i=0; i<N; i++ ) {
+            float la = 48.139126f;
+            float lo = 11.580186f;
+            for ( int j=0; j<20; j++ ) {
+                la += (Math.random()-0.5)/30;
+                lo += (Math.random()-0.5)/30;
+            }
+            lat.add(la);
+            lon.add(lo);
         }
+
+        for ( int i=0; i<N; i++ ) {
+            fMap.addMarker( new MarkerOptions()
+                    .position( new LatLng(lat.get(i), lon.get(i)) )
+                    .title("Title "+i)
+                    .snippet("Text "+i)
+                    );
+        }
+        */
     }
+
 }
