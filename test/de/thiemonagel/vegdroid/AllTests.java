5d5cba3086bfef38f35be1ca8f15c2d9d53a5eb4
Thiemo Nagel <thiemo.nagel@gmail.com>
2013-01-24 13:11:22 +0100

    Add JUnit tests for Rfc3339

---
+++ test/de/thiemonagel/vegdroid/AllTests.java
@@ -0,0 +1,11 @@
+package de.thiemonagel.vegdroid;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({Rfc3339Test.class})
+public class AllTests {
+
+}
