0a6e272077e90269806ac3853fbbfdf2185e7ef5
Thiemo Nagel <thiemo.nagel@gmail.com>
2013-02-25 17:07:58 +0100

    Implement caching in CachingGeoCoder

--- src/de/thiemonagel/vegdroid/CachingGeoCoder.java
+++ src/de/thiemonagel/vegdroid/CachingGeoCoder.java
@@ -3,6 +3,8 @@ package de.thiemonagel.vegdroid;
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
@@ -17,15 +19,17 @@ import com.google.android.gms.maps.model.LatLng;
 
 /**
  *
- * TODO: Implement caching.
+ * TODO: Make caching persistent across app restarts.
  *
  * TODO: At some point this could be further abstracted, avoiding any reference to Venue,
  * making it re-usable in other apps.
  *
  */
 public class CachingGeoCoder {
+    private static final long EXPIRY = 1000l * 3600 * 24 * 29;  // expire after 29 days for compliance with Google Maps API ToS 10.1.3 (b)
 
-    private ThreadPoolExecutor mExec = null;
+    private ThreadPoolExecutor          mExec  = null;
+    private Map<String,LatLngTimestamp> mCache = new TreeMap<String,LatLngTimestamp>();  // TreeMap uses less memory, I hope
 
     public CachingGeoCoder() {
         if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ) {
@@ -33,10 +37,22 @@ public class CachingGeoCoder {
         }
     }
 
-    void Resolve( Context context, Venue v ) {
+    public synchronized void Resolve( Context context, Venue v ) {
+        LatLngTimestamp llt = mCache.get(v.locString());
+        if ( llt != null ) {
+            if ( llt.expired() ) {
+                mCache.remove(v.locString());
+            } else {
+                // warning: duplicated code!
+                MapActivity ma = Global.getInstance(context).mapActivity;
+                if ( ma == null ) return;
+                ma.addMarker( context, v.getId(), llt.ll );
+            }
+        }
+
         // work around the fact that by default, multiple AsyncTasks are run
         // *sequentially* on honeycomb or later
-        LoadGeoCode lgc = new LoadGeoCode( context );
+        LoadGeoCode lgc = new LoadGeoCode( context, this );
         if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ) {
             Log.d( Global.LOG_TAG, "Queue LoadGeoCode in executor." );
             lgc.executeOnExecutor( mExec, v.locString(), String.valueOf( v.getId() ) );
@@ -46,19 +62,40 @@ public class CachingGeoCoder {
         }
     }
 
+    public synchronized void AddEntry( String s, LatLng ll ) {
+        LatLngTimestamp llt = new LatLngTimestamp( ll );
+        mCache.put( s, llt );
+    }
+
+    private class LatLngTimestamp {
+        public LatLng ll;
+        public Date   expiry;  // UTC
+        public LatLngTimestamp ( LatLng LL ) {
+            ll     = LL;
+            expiry = new Date();
+            expiry.setTime( expiry.getTime() + EXPIRY );
+        }
+        public boolean expired() {
+            Date now = new Date();  // UTC
+            return expiry.before(now);
+        }
+    }
+
 }  // class CachingGeoCoder
 
 
 class LoadGeoCode extends AsyncTask<String, Void, LatLng> {
     private volatile static int sGeoCount = 0;
 
-    private          Context  mContext;
-    private          Geocoder mGC;
-    private volatile int      mVenueId;
+    private          Context         mContext;
+    private          Geocoder        mGC;
+    private          CachingGeoCoder mCGC;
+    private volatile int             mVenueId;
 
-    public LoadGeoCode( Context context ) {
+    public LoadGeoCode( Context context, CachingGeoCoder CGC ) {
         mContext = context.getApplicationContext();
         mGC      = new Geocoder( context );
+        mCGC     = CGC;
     }
 
     @Override
@@ -82,7 +119,9 @@ class LoadGeoCode extends AsyncTask<String, Void, LatLng> {
 
             if ( la.size() > 0 ) {
                 Address a = la.get(0);
-                return new LatLng(a.getLatitude(), a.getLongitude());
+                LatLng ll = new LatLng(a.getLatitude(), a.getLongitude());
+                mCGC.AddEntry( loc, ll );
+                return ll;
             }
         } catch (IOException e) {
             Log.e( Global.LOG_TAG, "geocode #" + count + " io exception" );
@@ -95,9 +134,9 @@ class LoadGeoCode extends AsyncTask<String, Void, LatLng> {
     protected void onPostExecute( LatLng ll ) {
         if ( ll == null ) return;  // catch network errors, etc.
 
+        // warning: duplicated code!
         MapActivity ma = Global.getInstance(mContext).mapActivity;
         if ( ma == null ) return;
-
         ma.addMarker( mContext, mVenueId, ll );
     }
 }  // class LoadGeoCode
